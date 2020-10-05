b4ab2f1b755b1b4676713e6edc606b0d0501eece
keisukethewinter <addict20fx1@gmail.com>
2020-10-05 22:37:30 +0900

    Update card.js
    # 7~8行目
    カード情報の取得先を設定
    
    # 18行目
    通信が成功したらという条件から始める
    
    # 19,20
    定数を順に定義
    
    # 21
    "tokenObj"はトークンの情報を含んでいる。ブラウザ上に表示する必要がないので"type="hidden""と記述する。
    
    # 22
    フォームの"beforeend"(一番最後)にtokenObj(tokenが含まれたinput要素)を追加。"renderDom" は "charge-form" という id の要素(つまりカード情報の入力フォーム)

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -3,5 +3,24 @@ const pay = () => {
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("number"),
+      cvc: formData.get("cvc"),
+      exp_month: formData.get("exp_month"),
+      exp_year: `20${formData.get("exp_year")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status === 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form"); //idを元に要素を取得
+        const tokenObj = `<input value = ${token} type="hidden" name='card_token'>`; //paramsの中にトークンを含める
+        renderDom.insertAdjacentHTML("beforeend", tokenObj); //フォームの一番最後に要素を追加
+      }
+    });
   });
 };
