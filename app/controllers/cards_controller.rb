06a39522323b56a651a3f01d2db32c05f8f6a66a
keisukethewinter <addict20fx1@gmail.com>
2020-10-06 00:18:29 +0900

    Update cards_controller.rb
    カード登録後はトップページへリダイレクトするよう追記。

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -6,7 +6,7 @@ class CardsController < ApplicationController
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     customer = Payjp::Customer.create(
       description: 'test', #テストカードであることを説明
-    　card: params[:card_token]
+      card: params[:card_token]
     )
 
     card = Card.new(
@@ -14,6 +14,11 @@ class CardsController < ApplicationController
       customer_token: customer.id, # 顧客ID
       user_id: current_user.id
     )
+    if card.save
+      redirect_to root_path
+    else
+      redirect_to "new"
+    end
   end
 
 end
