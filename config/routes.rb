d35b24b5334b23d1a3e9b4a35ee53452e1a2c8e6
keisukethewinter <addict20fx1@gmail.com>
2020-10-05 18:47:59 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'items#index'
 
   resources :users, only: [:show, :update]
+  resources :cards, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
