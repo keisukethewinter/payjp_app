1edd60892cb528c01f4e6a317d4ebb8af09524ff
keisukethewinter <addict20fx1@gmail.com>
2020-10-06 01:13:32 +0900

    Update items_controller.rb
    商品購入機能は商品idを保存する必要があります。
    今回はprivateメソッド内でidを元に商品を特定する記述を追記。
    また、特定する必要があるのはorderメソッドが動く時だけなので、2行目で"only: :order"。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,17 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: :order
+
   def index
     @items = Item.all
   end
+
+  def order　# 購入のためのアクション
+  end
+
+  private
+
+  def find_item
+    @item = Item.find(params[:id]) # 購入する商品の特定
+  end
+  
 end
