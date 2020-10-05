12b1df7c3be01c4d3357847e209a8efafda362f3
keisukethewinter <addict20fx1@gmail.com>
2020-10-05 23:04:09 +0900

    Update cards_controller.rb
    PAY.JPリファレンス:
    https://pay.jp/docs/api/#%E6%94%AF%E6%89%95%E3%81%84%E3%83%AA%E3%82%B9%E3%83%88%E3%82%92%E5%8F%96%E5%BE%97
    
    # 6行目
    PAY.JPのメソッドを利用するために環境変数を読み込む
    
    # 7~9行目
    顧客に紐づいているカードの情報を生成

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,13 @@
 class CardsController < ApplicationController
   def new
   end
+
+  def create
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    customer = Payjp::Customer.create(
+      description: 'test', #テストカードであることを説明
+    　card: params[:card_token]
+    )
+  end
+  
 end
